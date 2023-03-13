d3febddbda7c42809c1297735a42ac3392618237
HajiMohamedRufai <60290402+HajiMohamedRufai@users.noreply.github.com>
2023-03-13 08:03:28 +0300

    Revert "Add greetings.py in working-together branch"

--- greetings.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-name = input("What is your name? ")
-hour = datetime.now().time().hour
-greeting = "Good morning" if 4 <= hour < 12 else "Good afternoon" if 12 <= hour < 16 else "Good night"
-print(f"{greeting}, {name}!")
